071f71c2f138d62e2fdfd7f41da773fa39d1c071
eifory <bartem007@gmail.com>
2020-11-19 11:43:48 +0300

    fix server

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const fastify = require('fastify')({ logger: true })
 
-const PORT = process.env.PORT || 5000;
-
 fastify.get('/', async (req, reply) => {
   return { hello: 'world' };
 })
 
 const start = async () => {
   try {
-    await fastify.listen(PORT);
+    await fastify.listen(process.env.PORT || 5000);
   } catch (err) {
     console.error(err);
     process.exit(1);
