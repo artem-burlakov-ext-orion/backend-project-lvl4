2823192d2c11c3aafdcf912ba970338d45209d5c
eifory <bartem007@gmail.com>
2020-11-20 08:36:05 +0300

    add host

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
 const app = fastify({ logger: true });
 
@@ -10,7 +11,7 @@ app.get('/', async (req, reply) => {
 
 const start = async () => {
   try {
-    await app.listen(port);
+    await app.listen(port, host);
   } catch (err) {
     console.error(err);
     process.exit(1);
